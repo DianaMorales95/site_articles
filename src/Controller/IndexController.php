df9f78f6e2c582a7f56a685fd0c432d20d9e7dae
Diana Morales <diana.nava0195@gmail.com>
2020-04-06 20:52:47 -0500

    obtention d'article

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,16 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class IndexController extends AbstractController
 {
-    /**
-     * @Route("/index", name="index")
-     */
-    public function index()
+    
+    /*public function index()
     {
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+        ]);
+    }*/
+
+
+    /**
+     * @Route("/index", name="index" )
+     */
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('index/index.html.twig', [
+        
+            'articles' => $articleRepository->findAll(),
         ]);
     }
 }
